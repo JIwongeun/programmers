b1d82d6d8cae890eb4b471308f28edf0f6811082
JIwongeun <jiwongeun321@naver.com>
2026-09-19 18:00:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = num_list
+    
+    ln, pn = num_list[len(num_list)-1], num_list[len(num_list)-2]
+    
+    if ln > pn:
+        answer.append(ln-pn)
+    else:
+        answer.append(ln*2)
+    
+    return answer
