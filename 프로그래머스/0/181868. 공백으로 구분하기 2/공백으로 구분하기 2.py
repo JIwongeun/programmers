c5d05a529f15391d7325c3eae6ebedb4dec64304
JIwongeun <jiwongeun321@naver.com>
2026-09-19 17:41:50 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -1,19 +1,4 @@
 def solution(my_string):
     
-    answer = []
-    ch = ""
     
-    for i, s in enumerate(my_string):
-        if s==" ":
-            if ch != "":
-                answer.append(ch)
-                ch = ""
-            continue
-        else:
-            ch += s
-            
-    if ch != "":
-         answer.append(ch)   
-    
-    
-    return answer
+    return my_string.split()
