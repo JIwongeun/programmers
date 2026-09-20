3578311c8308739166d59d73685655c67cf08225
JIwongeun <jiwongeun321@naver.com>
2026-09-20 16:56:36 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,6 +1,5 @@
 def solution(arr, queries):
-    for q in queries:
-        i1, i2 = q[0], q[1]
-        arr[i1], arr[i2] = arr[i2], arr[i1]
+    for a, b in queries:
+        arr[a], arr[b] = arr[b], arr[a]
     
     return arr
