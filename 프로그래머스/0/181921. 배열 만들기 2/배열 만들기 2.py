bad6faa4b6718dae68fcbfe3fb41a3cba44ee24f
JIwongeun <jiwongeun321@naver.com>
2026-09-20 19:53:57 +0900

    [level 0] Title: 배열 만들기 2, Time: 299.95 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,9 +2,9 @@ def solution(l, r):
     answer = []
 
     for n in range(l,r+1):
-        key1 = {"0","5"}
-        key2 = {"5"}
-        if set(str(n)) == key1 or set(str(n)) ==key2:
+        key = {"0","5"}
+        
+        if not (set(str(n)) - key):
             answer.append(n)
     
     return answer if answer else [-1]
