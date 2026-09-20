89d7bcd94c0c797f2e85cba701bbd3abc352d5f5
JIwongeun <jiwongeun321@naver.com>
2026-09-20 20:06:26 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,17 @@
+def solution(n):
+    
+    answer = [n]
+    
+    while(1):
+        if n%2==0:
+            n=n/2
+        else:
+            n=3*n+1
+        
+        answer.append(n)
+        
+        if n==1:
+            break
+    
+    
+    return answer
