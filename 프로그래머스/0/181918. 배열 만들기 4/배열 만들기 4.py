09d24ec2aabcc87dedbb587ec121ede2a2bf1356
JIwongeun <jiwongeun321@naver.com>
2026-09-20 20:25:19 +0900

    [level 0] Title: 배열 만들기 4, Time: 9.04 ms, Memory: 15.3 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,13 +1,9 @@
 def solution(arr):
     stk = []
-    i=0
     
-    while i<len(arr):
-        if not stk or stk[-1] < arr[i]:
-            stk.append(arr[i])
-            i+=1
-        else:
+    for i in range(len(arr)):
+        while stk and stk[-1]>=arr[i]:
             stk.pop()
+        stk.append(arr[i])
           
-            
     return stk
