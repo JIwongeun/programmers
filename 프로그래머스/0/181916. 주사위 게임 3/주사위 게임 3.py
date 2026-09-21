32018b482255a5e24571e8e28679c60dbc0de41e
JIwongeun <jiwongeun321@naver.com>
2026-09-21 23:19:29 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,27 +1,18 @@
-from collections import Counter
-
 def solution(a, b, c, d):
-   
-    answer = 0
+    nums = [a,b,c,d]
+    count = [nums.count(i) for i in nums]
     
-    data = sorted(Counter([a,b,c,d]).items(), key = lambda x : x[1])
-   
-    if len(data) == 1:
-        answer += 1111*data[0][0]
-    elif len(data) == 2:
-        if data [0][1] != data[1][1]:
-            if data[0][1] > data [1][1]:   
-                b = data[0][0]
-                s = data[1][0]
-            else:
-                b = data[1][0]
-                s = data[0][0]
-            answer += (10*b + s)**2
+    if max(count) ==4:
+        return 1111*a
+    elif max(count) == 3:
+        p = nums[count.index(3)]
+        q = nums[count.index(1)]
+        return (10*p + q)**2
+    elif max(count) == 2:
+        if min(count) == 2:
+            return (a+b)*abs(a-b) if a!=b else (a+c)*abs(a-c)
         else:
-            answer += (data[0][0] + data[1][0]) * abs((data[0][0] - data[1][0]))
-    elif len(data) == 3:
-        answer += data[0][0] * data[1][0]
+            p = nums[count.index(2)]
+            return (a*b*c*d) / p**2
     else:
-        answer += min(a,b,c,d)
-    
-    return answer
+        return min(nums)
