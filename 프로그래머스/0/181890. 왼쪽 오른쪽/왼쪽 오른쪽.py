dc8449235437619ff4910cee978646635748603d
JIwongeun <jiwongeun321@naver.com>
2026-09-27 00:52:01 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,13 +1,12 @@
 def solution(sl):
 
-    if "l" in sl:
-        if "r" in sl:
-            return sl[:sl.index("l")] if sl.index("l") < sl.index("r") else sl[sl.index("r")+1:]
-        else:
-            return sl[:sl.index("l")]
-    
-    if "r" in sl:
-        return sl[sl.index("r")+1:]
+    for i, s in enumerate(sl):
+        if s == "l":
+            return sl[:i]
+        if s == "r":
+            return sl[i+1:]
         
-    
+        if "l" not in sl and "r" not in sl:
+            break
+        
     return []
