0b9ef04bf182e7de60d4d98bff8d907f39f7f295
JIwongeun <jiwongeun321@naver.com>
2026-09-27 01:21:53 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,3 +1,3 @@
 def solution(todo_list, finished):
     
-    return [not_finished for i, not_finished in enumerate(todo_list) if not bool(finished[i])]
+    return [not_finished for i, not_finished in enumerate(todo_list) if not finished[i]]
